601a417843689c4c5892ad9d28212546e9c439cb
Pedro Felix dos Reis <136865593+zPedro19@users.noreply.github.com>
2026-04-25 19:45:17 -0300

    fix: ajusta testes unitários

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,20 +1,23 @@
+import pytest
 from src.calculadora import soma, subtracao, multiplicacao, divisao
+
 
 def test_soma():
     assert soma(2, 3) == 5
 
+
 def test_subtracao():
     assert subtracao(5, 3) == 2
+
 
 def test_multiplicacao():
     assert multiplicacao(2, 4) == 8
 
+
 def test_divisao():
     assert divisao(10, 2) == 5
 
+
 def test_divisao_por_zero():
-    try:
+    with pytest.raises(ZeroDivisionError):
         divisao(10, 0)
-        assert False
-    except ZeroDivisionError:
-        assert True
